21cab7bf284a7d2a9ab15480de5b9ed3a78b480b
SeunghunChae <48344785+SeunghunChae@users.noreply.github.com>
2023-01-06 14:08:06 +0900

    id/pw기능 수정

--- src/main/resources/static/mainscript.js
+++ src/main/resources/static/mainscript.js
@@ -9,10 +9,12 @@ function funcSubmit(){
 	
 	if(id.value.includes('@')==false)
 		alert(id.validationMessage);
-	else if(idArr.split(/[@|.]/)[idArr.split(/[@|.]/).length-1]!='com'
-		   || (idArr.split(/[@|.]/)[idArr.split(/[@|.]/).length-2]!='co' && idArr.split(/[@|.]/)[idArr.split(/[@|.]/).length-1]!='kr' && idArr.split(/[@|.]/)[idArr.split(/[@|.]/).length-1]!='com')
+	else if(idArr[idArr.length-1]!='com'
+		   || (idArr[idArr.length-2]!='co' && idArr[idArr.length-1]!='kr' && idArr[idArr.length-1]!='com')
 		   || idArr.length!=3)
 		alert('이메일 형식을 다시 확인해주세요.');
 	else if(id.value=='shchae822@naver.com' && pw=='1234')
 		window.open('main.html');
+	else
+	    alert('id와 password를 확인해주세요!');
 }
