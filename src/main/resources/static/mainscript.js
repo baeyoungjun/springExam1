978bf740457dd91891950f18f47b078d3579dcf8
SeunghunChae <48344785+SeunghunChae@users.noreply.github.com>
2023-01-17 13:12:27 +0900

    onload에 전역변수 선언

--- src/main/resources/static/mainscript.js
+++ src/main/resources/static/mainscript.js
@@ -1,5 +1,24 @@
+var clicked='';
+
 window.onload = function(){
  	console.log(location.href);
+
+
+    //클릭 이벤트
+    //var clicked='';
+
+    var target=document.getElementById("cardList");
+    //target.childNodes[1].childNodes[1].childNodes[1].textContent;
+
+    for(var i =0;i<(target.childNodes.length-1)/2;i++){
+        target.childNodes[i*2+1].addEventListener("click",function(){
+            clicked=target.textContent;
+            var hide=document.getElementById("firstTab");
+            var show=document.getElementById("content");
+            show.style.display='';
+            hide.style.display='none';
+        });
+    }
   }
 
 function funcSubmit(){
